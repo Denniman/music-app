5f57c7ec35130616dc28ea58c2472823d5979dd0
Dennis <dennimanadams@gmail.com>
2021-06-26 15:35:15 +0100

    add asset -add react-router link to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 import {FooterContainer} from './FooterStyle'
 
 export const Footer = () => {
     return (
         <FooterContainer>
-            <p>Made with &hearts; by <a href="https://github.com/denniman">Dennis Terna</a></p>
+            <p>
+                Made with &hearts; by <Link target="_blank" to="https://github.com/denniman">Dennis Terna</Link>
+            </p>
+            
         </FooterContainer>
     )
 }
