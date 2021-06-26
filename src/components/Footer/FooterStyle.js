d472a0c4c91b1dcc4421aeeb5a088de31fc0f84a
Dennis <dennimanadams@gmail.com>
2021-06-26 15:04:54 +0100

    change footer background color -change footer color -add hoover effect to footer achor link

--- src/components/Footer/FooterStyle.js
+++ src/components/Footer/FooterStyle.js
@@ -2,10 +2,10 @@ import styled from 'styled-components'
 
 
 export const FooterContainer = styled.footer`
-    background-color: var(--theme-color);
+    background-color: #f9f9f9;
     grid-area: footer;
     padding: var(--padding);
-    color: #fff;
+    color: #252c23;;
     text-align: center;
     font-size: 1.8rem;
     letter-spacing: .3rem;
